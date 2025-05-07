9fa13efc234a45cc6309109049a0cc43556b2bd7
divyansh-git-max <divyanshajay@gmail.com>
2025-05-07 15:44:04 +0530

    Intent-classification model

--- Models/intent_model.py
+++ Models/intent_model.py
@@ -1,60 +1,40 @@
 # intent classification using the logistic regression 
 
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import accuracy_score,classification_report
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import re
 import joblib
-
-
-
+from flask import jsonify
 
 class IntentClassifier():
-    def __init__(self,model_path='intent_model'):
-        self.model=None
-        self.vectorizer=None
-        try:
-            self.model = joblib.load(self.model_path)
-            print("[INFO] Model loaded from disk.")
-        except FileNotFoundError:
-            print("[INFO] No saved model found. Train a new one.")
-
-    def regex_search(self,data):
-        text,labels=zip(*data)
-        text=text.lower()
-        if re.search(r'\bhi\b|\bhello!\b|\bhey\b|\bhello\b|\bmorning\b|\bevening\b|\bevening\b',text):
+    def __init__(self,model_path='Intent_model'):
+        self.model = joblib.load(model_path)
+        print("[INFO] Model loaded from disk.")
+    
+    def regex_search(self,user_query):
+        if re.search(r'\bhi\b|\bhello!\b|\bhey\b|\bhello\b|\bmorning\b|\bevening\b|\bevening\b',user_query):
             return "greeting"
-        elif re.search(r'\bbuy\b.*\bshare\b',text):
+        elif re.search(r'\bbuy\b.*\bshare\b',user_query):
             return 'buy_order'
-        elif re.search(r'\bsell\b.*\bshare\b',text):
+        elif re.search(r'\bsell\b.*\bshare\b',user_query):
             return 'sell_order'
-        elif re.search(r'\bcheck\b.*\bbalance\b',text):
+        elif re.search(r'\bcheck\b.*\bbalance\b',user_query):
             return 'wallet_fund_query'
-        elif re.search(r'\bshow\b.*\bwallet\b.*\bfund\b',text):
+        elif re.search(r'\bshow\b.*\bwallet\b.*\bfund\b',user_query):
             return 'wallet_fund_query'
 
-    def model_training(self,data):
-        texts,labels=zip(*data)
-        X_train,X_val,y_train,y_val=train_test_split(texts,labels,test_size=0.2,random_state=42)
-        self.model=Pipeline([
-            ('tfidf',TfidfVectorizer()),
-            ('LG',LogisticRegression(max_iter=10000))
-        ])
-        self.model.fit(X_train,y_train)
-        print("Training complete. Evaluation on test set:")
-        joblib.dump(self.model, self.model_path)
-        y_pred=self.model.predict(X_val)
-        print('classifiction_report',classification_report(y_val,y_pred))
 
-    def predict(self,data):
-        rule_based_intent=self.regex_search(data)
-        text,labels=zip(*data)
-        text=text.lower()
+    def predict(self,user_query):
+        rule_based_intent=self.regex_search(user_query)
         if rule_based_intent:
             return rule_based_intent
-        return self.model_training.predict(text)[0]
+        final_intent = self.model.predict([user_query])
+        if final_intent:
+            return final_intent
+        return jsonify({
+            "message":"intent not found"
+        }),400
     
 
 
--- app.py
+++ app.py
@@ -1,15 +1,36 @@
 from flask import Flask,request,jsonify
 import spacy
-import re 
+import re
+import numpy as np
 from bs4 import BeautifulSoup
 from Models.ner_model import NERModel
 from Models.intent_model import IntentClassifier
-nlp = spacy.load("custom_ner_model") # our custom ner model trained with dataset
-app=Flask(__name__)
+import chromadb
 
+
+
+
+nlp = spacy.load("custom_ner_model") 
+
+app=Flask(__name__)
 trained_nlp=spacy.load('custom_ner_model')
 
 
+# chroma_client = chromadb.Client()
+# collection = chroma_client.create_collection(name="chatbot_faq")
+
+
+# collection.add(
+#     documents=[
+#         "How to cancel the order?",
+#         "I want the refund"
+#         "I want to know the price of META stock"
+#         "I want to know the stock price of the apple company",
+#         "This document is about the oranges",
+#         "Give my P/L statement for this month"
+#     ],
+#     ids=["id1", "id2","id3","id4","id5",'id6']
+# )
 
 # preprocess the user input => predict the entity =>  print the entity => generate the response
 
@@ -67,37 +88,23 @@ trained_nlp=spacy.load('custom_ner_model')
 
 
 
+# def generate_response(user_input): # entities=[]
+#     for text in user_input:
+#         doc=trained_nlp(text)
 
 
 
+# @app.route('/chat',methods=["GET","POST"]) # final O/P
+# def chat():
+#     data=request.get_json()
+#     user_input=[data.get('text')]
+#     response=generate_response(user_input)
+#     return jsonify({
+#         "message":response
+#     }),200
 
-def generate_response(user_input): # entities=[]
-    for text in user_input:
-        doc=trained_nlp(text)
-
-
-
-
-
-
-
-
-@app.route('/chat',methods=["GET","POST"]) # final O/P
-def chat():
-    data=request.get_json()
-    user_input=[data.get('text')]
-    response=generate_response(user_input)
-    return jsonify({
-        "message":response
-    }),200
-
-
-# ner=NERModel()
-# ner.print_entities('I want buy the AAPL stocks')
-
-# ner.print_batch(text)
  
-@app.route('/ner_testing_route',methods=['POST',"GET"]) # extract the entities from the user input
+@app.route('/ner_testing_route',methods=['POST',"GET"]) # route to extract the enitites only
 def ner_testing_route(): 
     data=request.get_json()
     user_input=data.get('text')
@@ -110,26 +117,71 @@ def ner_testing_route():
         })
     else:
         return jsonify({
-            "message":"Sorry i Could get you can you rephrase your answer please"
+            "message":"Sorry i Could get you can you rephrase your query please"
         })
     
 
 
 
-
-
-@app.route('/intent_classifier',methods=['GET',"POST"])
-def intent_classifer():
+@app.route('/intent_classify',methods=['GET',"POST"]) # route to test intent
+def intent_classify():
     data=request.get_json()
-    intent=IntentClassifier()
     user_input=data.get('text')
+    if not user_input:
+        return jsonify({
+            "message":"Sorry you have to type the query"
+        }),400
+    intent_classifer=IntentClassifier()
+    user_intent=intent_classifer.predict(user_input)
+    if isinstance(user_intent,np.ndarray):
+        user_intent=user_intent.tolist()
+    if user_intent:
+        return jsonify({
+            "message":user_intent[0]
+        }),200
+    else:
+        return jsonify({
+            "message":"can you rephrase your query?"
+        }),400
+
+
+# @app.route('/chromadb',methods=['GET',"POST"])
+# def chromadb():
+
+#     return 
+
+
+
+@app.route('/preprocess_query',methods=["GET","POST"]) 
+def preprocess_query():
+    data=request.get_json()
+    user_input=data.get('text')
+    if not user_input:
+        return jsonify({
+            "message":"Sorry you have to type the query"
+        }),400
+    ner=NERModel()
+    final_dict=dict()
+    intent_classifer=IntentClassifier()
+    extracted_entity=ner.extract_entities(user_input)
+    user_intent=intent_classifer.predict(user_input)
+    if isinstance(user_intent,np.ndarray):
+        user_intent=user_intent.tolist()
+    if user_intent:
+        if "intent" not in final_dict:
+            final_dict['intent']=user_intent
+        if extracted_entity != []:
+            if 'entities' not in final_dict:
+                final_dict['entities']=extracted_entity
+        
+        return jsonify({
+            "message":final_dict
+        }),200
     return jsonify({
-        "message":
-    })
-
-
-
-
+        "message":"Sorry couldn't get you please rephrase the query and ask again"
+    }),400
 
 if __name__ =="__main__":
     app.run(debug=True)
+
+
